69b76edef974508dae5bf2d716f42312145bb881
Shekhar Chauhan <shekharbarca10@gmail.com>
2024-04-14 19:24:02 +0530

    Initial Commit

---
+++ NotificationTester/NotificationTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTesterApp.swift
+//  NotificationTester
+//
+//  Created by Shekhar Chauhan on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NotificationTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
